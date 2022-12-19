d6460712850dc0f1df0ccf5867d824eeac2cbd0e
pholophus <iqbalkisas6@gmail.com>
2022-12-19 13:48:50 +0800

    first commit

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
-const app = require('express')();
+const express = require('express')
+const path = require('path')
+const app = express()
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
+const multer = require('multer')
+const fs = require("fs");
+
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+app.post('/fileupload', multer().single('file'), (req, res) => {
+  // res.sendFile(__dirname + '/index.html');
+  console.log(req)
+  console.log(req.file)
+  fs.writeFile("public/result/Oasis_spine_1.0_3.png", req.file.buffer, function(err) {
+    if(err) {
+        return console.log(err);
+    }
+    console.log("The file was saved!");
+}); 
+});
+
 io.on('connection', (socket) => {
-  socket.on('chat message', msg => {
-    io.emit('chat message', msg);
+  socket.on('chat message trigger', msg => {
+    io.emit('chat message receiver', msg);
   });
 });
 
